e8a5ab97bde02d3f464451b4cbf1379f89b5efa1
vqlai <695850246@qq.com>
2017-08-27 11:02:53 +0800

    add config file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+
+const app = express();
+app.use('/static', express.static(`${__dirname}/static`));
+app.use('/v1', proxy({
+  target: 'https://www.dongqiudi.com',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/v1': ''
+  }
+}));
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+app.listen(7000);
