8f26df03c32dba84e36ba20f4de1b02af511a057
vqlai <695850246@qq.com>
2017-08-30 21:16:29 +0800

    add new file

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <top></top>
+    <tab></tab>
+    <transition name="fade">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+import Top from 'components/top/top'
+import Tab from 'components/tab/tab'
+export default {
+  name: 'app',
+  data() {
+    return{
+    }
+  },
+  methods: {
+  },
+  components: {
+    Top,
+    Tab
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+#app
+  text-align: center
+  
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,39 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import store from './store' // 没有指定文件名，默认引入文件夹下的index文件
+// import axios from 'common/js/http.js'
+// import axios from 'axios'
+// Vue.prototype.$axios = axios
+
+// 将fastclick挂载到body上，解决所有元素的点击事件的300ms延迟问题
+fastclick.attach(document.body)
+
+// 延迟加载
+Vue.use(VueLazyload, {
+	preLoad: 1.3,
+	error: require('common/images/lazyload.png'),
+	loading: require('common/images/lazyload.png'),
+	attempt: 1
+})
+
+// 导入默认样式
+import 'common/stylus/index.styl'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store, 
+  render: h => h(App)
+})
+
+// template: '<App/>',
+// components: { App }
